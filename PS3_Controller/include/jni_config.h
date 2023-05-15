9bff7e7f5970b3e0001928efccaeef7948d2a5c0
mopore <jni@mopore.org>
2023-05-15 12:29:46 +0200

    Switch base with control.

--- PS3_Controller/include/jni_config.h
+++ PS3_Controller/include/jni_config.h
@@ -3,7 +3,7 @@
 #if WIFI_SETUP == 1
 	#define WIFI_SSID      "Loxodonta"
 	#define WIFI_PASS      "Twitch7%Carton%Driller%Bluish"
-	#define UDP_RECEIVER_SOCKET_IP "192.168.199.240"  // Test Rig
+	#define UDP_RECEIVER_SOCKET_IP "192.168.199.121"  // Test Rig
 #elif WIFI_SETUP == 2
 	#define WIFI_SSID	  "Talpa"	
 	#define WIFI_PASS	  "#K9bC5PRON3Lr3MQmJZtLgB#ax*Tr2kK96yb^83g&lfK%Te2F7Soai2TqPgJA&u"
