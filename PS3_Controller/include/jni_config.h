65f4127ef12bf7f7b94b5ca6acbaff2efa48d524
mopore <jni@mopore.org>
2023-05-23 16:40:36 +0200

    Switching to Backup Proto Controller.

--- PS3_Controller/include/jni_config.h
+++ PS3_Controller/include/jni_config.h
@@ -3,11 +3,11 @@
 #if WIFI_SETUP == 1
 	#define WIFI_SSID      "Loxodonta"
 	#define WIFI_PASS      "Twitch7%Carton%Driller%Bluish"
-	#define UDP_RECEIVER_SOCKET_IP "192.168.199.121"  // Test Rig
+	#define UDP_RECEIVER_SOCKET_IP "192.168.199.233"  // Backup JNI Proto Car
 #elif WIFI_SETUP == 2
 	#define WIFI_SSID	  "Talpa"	
 	#define WIFI_PASS	  "#K9bC5PRON3Lr3MQmJZtLgB#ax*Tr2kK96yb^83g&lfK%Te2F7Soai2TqPgJA&u"
-	#define UDP_RECEIVER_SOCKET_IP "192.168.8.120"  // Talpa test rig
+	#define UDP_RECEIVER_SOCKET_IP "192.168.8.233"  // Talpa test rig
 #endif
 
 // #define UDP_RECEIVER_SOCKET_IP "192.168.199.201"  // Test Rig
